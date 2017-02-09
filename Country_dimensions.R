619e016ee135acae2e36a22ff2dfeb50e8aebd1b
Spencer <sl.graha@gmail.com>
2017-02-08 21:57:28 -0600

    final chart

--- Country_dimensions.R
+++ Country_dimensions.R
@@ -1,4 +1,4 @@
-require(dplyr); require(ggplot2); require(ggrepel); require(ggthem)
+require(dplyr); require(ggplot2); require(ggrepel); require(ggthemes)
 
 # read data into R
 data_file <- paste(getwd(), "/Country_dimensions.csv", sep = "")
@@ -11,12 +11,14 @@ data2 <- data %>% filter(complete.cases(data)) %>%
 # option to plot without Singapore (where I haven't visited)
 data3 <- data2 %>% filter(Country !="Singapore")
 
-plotdata <- data3
+plotdata <- data3 %>% mutate(visited = ifelse(Country == "Japan", "new", "visited"))
 
 g <- ggplot(plotdata, aes(USdiff_sad_norm, GDPPC)) +
         theme_classic() +
-        geom_point() +
-        geom_text_repel(label = plotdata$Country, segment.color = NA) +
+        geom_point(size = 2, aes(color = visited)) +
+        scale_color_manual(values = c("new" = "#b20101", "visited" = "#777777")) +
+        geom_text_repel(label = plotdata$Country, segment.color = NA,
+                        point.padding = unit(1, "pt"), nudge_y = .15) +
         scale_x_continuous(name = "Cultural Difference Index (vs. USA)",
                            breaks = c(0, 10, 20, 30, 40, 50),
                            labels = c("USA", "10", "20", "30", "40", "50")) +
@@ -28,10 +30,13 @@ g <- ggplot(plotdata, aes(USdiff_sad_norm, GDPPC)) +
         labs(title = "Economy and Culture", 
              subtitle = "Among countries I've visited",
              caption = "Based on data from the CIA World Factbook and Geert Hofstede") +
-        geom_hline(yintercept = 25000, linetype = "dashed", color = "dark grey") +
-        geom_vline(xintercept = 27, linetype = "dashed", color = "dark grey") +
+        geom_hline(yintercept = 28000, linetype = "dashed", color = "dark grey",
+                   size = .4) +
+        geom_vline(xintercept = 27, linetype = "dashed", color = "dark grey",
+                   size = .4) +
         theme(text = element_text(family = "sans", size = 12, color = "#3C3C3C"),
               plot.title = element_text(size = 16, face = "bold"),
               plot.caption = element_text(face = "italic", size = 10),
-              axis.title = element_text(face = "bold"))
+              axis.title = element_text(face = "bold"),
+              legend.position = "none")
 
