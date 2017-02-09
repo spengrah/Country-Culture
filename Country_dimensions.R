453385473dc26a9d24676cdf448d7cec17111362
Spencer <sl.graha@gmail.com>
2017-02-08 19:26:56 -0600

    initial plot

--- Country_dimensions.R
+++ Country_dimensions.R
@@ -1,9 +1,16 @@
-require(dplyr); require(ggplot2)
+require(dplyr); require(ggplot2); require(ggrepel)
 
 # read data into R
 data_file <- paste(getwd(), "/Country_dimensions.csv", sep = "")
 data <- read.csv(file = data_file)
 
-# remove incomplete cases (where some data was NA)
-data2 <- filter(data, complete.cases(data))
+# remove incomplete cases (where some data was NA) and the US
+data2 <- data %>% filter(complete.cases(data)) %>%
+                   filter(Country != "United States")
 
+g <- ggplot(data2, aes(USdiff_sad_norm, GDPPC)) +
+        theme_minimal() +
+        geom_point() +
+        geom_text_repel(label = data2$Country, segment.color = NA) +
+        scale_x_continuous(name = "Cultural Difference from USA",
+                           limits = c(0, 75))
