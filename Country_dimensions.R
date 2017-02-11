11bf8fb185510a25364f6775044583750d3a36af
Spencer <sl.graha@gmail.com>
2017-02-11 12:51:15 -0600

    cluster analysis

--- Country_dimensions.R
+++ Country_dimensions.R
@@ -1,4 +1,5 @@
 require(dplyr); require(ggplot2); require(ggrepel); require(ggthemes)
+require(cluster)
 
 # read data into R
 data_file <- paste(getwd(), "/Country_dimensions.csv", sep = "")
@@ -11,14 +12,53 @@ data2 <- data %>% filter(complete.cases(data)) %>%
 # option to plot without Singapore (where I haven't visited)
 data3 <- data2 %>% filter(Country !="Singapore")
 
-plotdata <- data3 %>% mutate(visited = ifelse(Country == "Japan", "new", "visited"))
+# try out some clustering
+# need complete cases, but include the US
+# also need to normalize GDPPC - use max GDPPC = $129,700 (Qatar)
+maxGDPPC <- 129700
+data4 <- data %>% filter(complete.cases(data)) %>%
+        filter(Country != "Singapore") %>%
+        mutate(GDPPC_norm = GDPPC/maxGDPPC*100)
+data5 <- data4 %>% select(2:7, 10, 11)
+row.names(data5) <- data4$Country
 
+
+# clusters with each of the 6 culture dimensions and normalized GDPPC
+data8 <- data5 %>% select(1:6, GDPPC_norm)
+cl1 <- kmeans(data8, 4)
+plot(data8, col = cl1$cluster) # see all the dims
+# observations: individualism drives the most amount of the differentiation
+plot(data6, col = cl1$cluster) # plot GDP vs. cultural index
+# observations: france gets separated from 
+
+
+# clusters with GDP and culture diff index
+data6 <- data5 %>% select(USdiff_sad_norm, GDPPC_norm)
+cl2 <- kmeans(data6, 4)
+plot(data6, col = cl2$cluster)
+points(cl2$centers, col = 1:4, pch = 8, cex = 2)
+# silhouette plot
+dist6 <- dist(data6)
+sil <- silhouette(cl$cluster, dist6)
+plot(sil)
+# observations: same groups as my quandrants!
+
+# clusters with GDP and individualism
+data7 <- data5 %>% select(IDV, GDPPC_norm)
+cl3 <- kmeans(data7, 4)
+plot(data7, col = cl3$cluster)
+
+# plot the economy and cultural index data
+plotdata <- data3 %>% mutate(visited = ifelse(Country == "Japan", "new", "visited"),
+                             cluster = factor(cl1$cluster[2:13])) # bring in full dims cluster info
 g <- ggplot(plotdata, aes(USdiff_sad_norm, GDPPC)) +
         theme_classic() +
-        geom_point(size = 2, aes(color = visited)) +
-        scale_color_manual(values = c("new" = "#b20101", "visited" = "#777777")) +
+        geom_point(size = 2, aes(color = cluster)) +
         geom_text_repel(label = plotdata$Country, segment.color = NA,
                         point.padding = unit(1, "pt"), nudge_y = .15) +
+        scale_color_manual(name = "Cluster",
+                           values = c("4" = "black", "2" = "green", "1" = "blue", "3" = "red"),
+                           labels = c("France", "Developing", "Japan", "Westerns")) +
         scale_x_continuous(name = "Cultural Difference Index (vs. USA)",
                            breaks = c(0, 10, 20, 30, 40, 50),
                            labels = c("USA", "10", "20", "30", "40", "50")) +
@@ -38,5 +78,6 @@ g <- ggplot(plotdata, aes(USdiff_sad_norm, GDPPC)) +
               plot.title = element_text(size = 16, face = "bold"),
               plot.caption = element_text(face = "italic", size = 10),
               axis.title = element_text(face = "bold"),
-              legend.position = "none")
-
+              legend.title = element_text(face = "bold"),
+              legend.justification = "top",
+              legend.background = element_rect(fill = "light grey"))
